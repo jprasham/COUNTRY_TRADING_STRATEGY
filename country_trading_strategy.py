e432e6484177ffdda60305aa33294915b0fc856e
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2025-08-30 16:18:11 +0800

    Update country_trading_strategy.py

--- country_trading_strategy.py
+++ country_trading_strategy.py
@@ -9,9 +9,9 @@ st.set_page_config(page_title='COUNTRY TRADING STRATEGY', page_icon=':bar_chart:
 st.header('COUNTRY TRADING STRATEGY')
 
 # Display the last update date
-st.markdown('#### Updated: 19/08/2025')
+st.markdown('#### Updated: 29/08/2025')
 
-excel_file = 'COUNTRY_TRADING_STRATEGY.xlsx'
+excel_file = 'COUNTRY_TRADING_STRATEGY_29.xlsx'
 sheet_name1 = 'FILTER1'
 sheet_name2 = 'FILTER2'
 use_cols = "A:I"                          
